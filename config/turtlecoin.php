6c1e0b5fbaacd2804b46f014738cecac465c99f5
Tim McLeod <tim@timmcleod.com>
2018-11-05 16:53:47 -0500

    Prefixed env vars with TC_

--- config/turtlecoin.php
+++ config/turtlecoin.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'daemon_host'                 => env('DAEMON_HOST', 'http://localhost'),
-    'daemon_port'                 => env('DAEMON_PORT', 11898),
-    'wallet_service_host'         => env('WALLET_SERVICE_HOST', 'http://localhost'),
-    'wallet_service_port'         => env('WALLET_SERVICE_PORT', 8070),
-    'wallet_service_rpc_password' => env('WALLET_SERVICE_RPC_PASSWORD'),
-    'wallet_directory'            => base_path(env('WALLET_DIRECTORY', '../wallet')),
-    'wallet_filename'             => env('WALLET_FILENAME', 'master.wallet'),
-    'wallet_password'             => env('WALLET_PASSWORD'),
+    'daemon_host'                 => env('TC_DAEMON_HOST', 'http://localhost'),
+    'daemon_port'                 => env('TC_DAEMON_PORT', 11898),
+    'wallet_service_host'         => env('TC_WALLET_SERVICE_HOST', 'http://localhost'),
+    'wallet_service_port'         => env('TC_WALLET_SERVICE_PORT', 8070),
+    'wallet_service_rpc_password' => env('TC_WALLET_SERVICE_RPC_PASSWORD'),
+    'wallet_directory'            => base_path(env('TC_WALLET_DIRECTORY', '../wallet')),
+    'wallet_filename'             => env('TC_WALLET_FILENAME', 'master.wallet'),
+    'wallet_password'             => env('TC_WALLET_PASSWORD'),
 ];
